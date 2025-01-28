f59fb06622c83c89741758b49049e90582f06d20
Igor Oliveira Duarte <igor.oliveiraduarte05@gmail.com>
2025-01-28 17:37:41 -0300

    p2

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -2,6 +2,38 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include "pratos.csv"
+#include "pratos.h"
 
-void bubbleSort
+void bubbleSort(Prato pratos[], int n){
+    for(int i=0 ; i<n-1; i++){
+        for(int j=0; j<n-i-1 ;j++ ){
+            if(pratos[j].prioridade > pratos[j + 1].prioridade ||
+                (pratos[j].prioridade == pratos[j + 1].prioridade && pratos[j].tempo > pratos[j + 1].tempo)) {
+                Prato temp= pratos[j];
+                pratos[j] = pratos[j+ 1];
+                pratos[j+1]= temp;
+            }
+        }
+
+    }
+}
+int main(){
+    
+    Prato pratosOrdenados[num_pratos];
+    memcpy(pratosOrdenados, pratos, sizeof(pratos));
+
+    clock_t start_time = clock();
+
+    bubbleSort(pratosOrdenados, num_pratos);
+    
+    clock_t end_time= clock();
+    
+    printf("\nPratos ordenados:\n");
+    for(int  i=0; i< num_pratos; i++){
+        printf("Prioridade: %d, Tempo: %d, Nome: %s\n", pratosOrdenados[i].prioridade, pratosOrdenados[i].tempo, pratosOrdenados[i].nome);
+    }
+    double elapsed_time = ((double)(end_time - start_time))/ CLOCKS_PER_SEC;
+    printf("Tempo de execução: %f segundos\n", elapsed_time);
+    return 0;
+
+}
--- Quick_sort.c
+++ Quick_sort.c
@@ -1,4 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "pratos.csv"
+#include <string.h>
+#include "pratos.h"
+
+void particao(Prato pratos[], int esq, int dir, int*i, int *j){
+    *i = esq;
+    *j = dir;
+    Prato pivo = pratos[(esq+dir)/2];
+
+    while(*i <= *j){
+        while (pratos[*i].prioridade < pivo.prioridade && *i <= dir){
+            (*i)++;
+        }
+        while (pratos[*j].prioridade > pivo.prioridade && *j >= esq){
+            (*j)--;
+        }
+        if(*i <= *j){
+            Prato aux = pratos[*i];
+            pratos[*i] = pratos[*j];
+            pratos[*j] = aux;
+            (*i)++;
+            (*j)--;
+        }
+    }
+    
+}
+void Quicksort(Prato pratos[], int esq, int dir){
+    int i, j;
+    particao(pratos, esq, dir, &i, &j);
+    if(i< dir){
+        Quicksort(pratos, i, dir);
+    }
+    if(j> esq){
+        Quicksort(pratos, esq, j);
+    }
+}
+
+int main(){
+    Prato pratosOrdenados[num_pratos];
+    memcpy(pratosOrdenados, pratos, sizeof(pratos[0])* num_pratos);
+
+    clock_t start_time= clock();
+    Quicksort(pratosOrdenados, 0, num_pratos-1);
+    clock_t end_time = clock();
+
+    printf("\nPratos ordenados:\n");
+    for(int  i=0; i< num_pratos; i++){
+        printf("Prioridade: %d, Tempo: %d, Nome: %s\n", pratosOrdenados[i].prioridade, pratosOrdenados[i].tempo, pratosOrdenados[i].nome);
+    }
+
+    double elapsed_time = ((double)(end_time - start_time))/ CLOCKS_PER_SEC;
+    printf("Tempo de execução: %f segundos\n", elapsed_time);
+    return 0;
+
+    
+}
